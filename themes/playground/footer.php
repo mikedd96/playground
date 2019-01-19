dc8b2dc724408cb004f70e12cf515cf63d5826f1
mike dunk <mike_4596@hotmail.com>
2019-01-18 20:36:21 -0800

    add content to footer... adn some style, shhh

---
+++ themes/playground/footer.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package startertheme
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+
+			<h2 class="call">Want a High Functioning Website that Looks Great?</h2>
+			<div class="text-links">
+			<a class="about">About Me.</a>
+			<a class="find-me">How to Find Me.</a>
+			<a class="my-work">My Work.</a>
+			<a class="resume"> My Resume.</a>
+			<p class="copyright"> © 2019 Michael Dunk.</p>
+</div>	
+<div class="send-email">	
+  Send Me an Email: <input type="text">
+  <input type="submit" value="Submit">
+</div>
+					
+		<a class="fab fa-linkedin-in" href="https://github.com/mikedd96"></a>
+		<a class="fab fa-github" href="https://www.linkedin.com/in/michael-dunk-991952175/"></a>
+		<p class="location"><a class="fas fa-map-marker-alt"></a>Vancouver, BC, Canada</p>
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
